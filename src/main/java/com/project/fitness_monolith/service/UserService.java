e69d988d8551897d1296204ab065a904c07dcc91
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-01 17:06:37 +0530

    - Created the "UserService". - Implemented the "register" functionality to save the user data in the DB.

---
+++ src/main/java/com/project/fitness_monolith/service/UserService.java
@@ -0,0 +1,37 @@
+package com.project.fitness_monolith.service;
+
+import com.project.fitness_monolith.dto.RegisterRequest;
+import com.project.fitness_monolith.dto.UserResponse;
+import com.project.fitness_monolith.entity.User;
+import com.project.fitness_monolith.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse register(RegisterRequest registerRequest) {
+        User user = new User(null,
+                registerRequest.getEmail(),
+                registerRequest.getPassword(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                List.of(),
+                List.of());
+
+        User savedUser = userRepository.save(user);
+
+        UserResponse userResponse = new UserResponse(savedUser.getId(), savedUser.getEmail(), savedUser.getPassword(), savedUser.getFirstName(), savedUser.getLastName());
+
+        return userResponse;
+    }
+
+}
