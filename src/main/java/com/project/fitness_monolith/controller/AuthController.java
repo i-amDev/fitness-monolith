8f1b94aea8dd33f7ac10e1e754f37e4922fc94af
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-01 17:10:48 +0530

    - Created the "AuthController". - Added the "POST" mapping for "register" functionality.

---
+++ src/main/java/com/project/fitness_monolith/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.project.fitness_monolith.controller;
+
+import com.project.fitness_monolith.dto.RegisterRequest;
+import com.project.fitness_monolith.dto.UserResponse;
+import com.project.fitness_monolith.entity.User;
+import com.project.fitness_monolith.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(userService.register(registerRequest));
+    }
+
+
+}
