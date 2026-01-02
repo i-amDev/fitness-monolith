a7f7e15aa683804c4f3ceaded211a8d9ae5a187c
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-02 12:24:12 +0530

    - Created the "ActivityRequest" dto class to take inputs from the users.

---
+++ src/main/java/com/project/fitness_monolith/dto/ActivityRequest.java
@@ -0,0 +1,27 @@
+package com.project.fitness_monolith.dto;
+
+import com.project.fitness_monolith.entity.ActivityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityRequest {
+
+    private String userId;
+
+    private ActivityType type;
+
+    private Map<String, Object> additionalMetrics;
+
+    private Integer duration;
+
+    private Integer caloriesBurned;
+
+    private LocalDateTime startTime;
+}
