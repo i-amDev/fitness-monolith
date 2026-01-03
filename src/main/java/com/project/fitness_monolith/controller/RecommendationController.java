c6a51f83adf8a067abe3d32ecf0230da5811d5c2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-03 14:18:26 +0530

    - RecommendationController  1. Added the "POST" mapping for "generateRecommendation" method to save the recommendations in the DB. - RecommendationService  1. Implemented the business logic for "generateRecommendation" method.

---
+++ src/main/java/com/project/fitness_monolith/controller/RecommendationController.java
@@ -0,0 +1,25 @@
+package com.project.fitness_monolith.controller;
+
+import com.project.fitness_monolith.dto.RecommendationRequest;
+import com.project.fitness_monolith.entity.Recommendation;
+import com.project.fitness_monolith.service.RecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recommendation")
+@RequiredArgsConstructor
+public class RecommendationController {
+
+    private final RecommendationService recommendationService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<Recommendation> generateRecommendation(@RequestBody RecommendationRequest recommendationRequest) {
+        return ResponseEntity.ok(recommendationService.generateRecommendation(recommendationRequest));
+    }
+
+
+}
---
+++ src/main/java/com/project/fitness_monolith/service/RecommendationService.java
@@ -0,0 +1,41 @@
+package com.project.fitness_monolith.service;
+
+import com.project.fitness_monolith.dto.RecommendationRequest;
+import com.project.fitness_monolith.entity.Activity;
+import com.project.fitness_monolith.entity.Recommendation;
+import com.project.fitness_monolith.entity.User;
+import com.project.fitness_monolith.repository.ActivityRepository;
+import com.project.fitness_monolith.repository.RecommendationRepository;
+import com.project.fitness_monolith.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+
+    private final RecommendationRepository recommendationRepository;
+
+    private final UserRepository userRepository;
+
+    private final ActivityRepository activityRepository;
+
+    public Recommendation generateRecommendation(RecommendationRequest recommendationRequest) {
+        User user = userRepository.findById(recommendationRequest.getUserId()).orElseThrow(() -> new RuntimeException("Invalid User " + recommendationRequest.getUserId()));
+
+        Activity activity = activityRepository.findById(recommendationRequest.getActivityId()).orElseThrow(() -> new RuntimeException("Invalid Activity " + recommendationRequest.getActivityId()));
+
+        Recommendation recommendation = Recommendation.builder()
+                .user(user)
+                .activity(activity)
+                .improvements(recommendationRequest.getImprovements())
+                .suggestions(recommendationRequest.getSuggestions())
+                .safety(recommendationRequest.getSafety())
+                .build();
+
+        return recommendationRepository.save(recommendation);
+    }
+}
