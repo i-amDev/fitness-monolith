bd3041ed0948921a4d9bb40d4673f4d8442437e9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-03 14:14:56 +0530

    - Added the "@Builder" annotation for "Recommendation" entity.

--- src/main/java/com/project/fitness_monolith/entity/Recommendation.java
+++ src/main/java/com/project/fitness_monolith/entity/Recommendation.java
@@ -3,6 +3,7 @@ package com.project.fitness_monolith.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,6 +18,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Recommendation {
 
     @Id
